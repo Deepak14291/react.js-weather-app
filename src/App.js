ad624da16e8bdcef73f6be59cc580558cc7d4589
Deepak Kodi <125197608+Deepak14291@users.noreply.github.com>
2025-01-13 13:52:11 +0000

    ui update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import { useRef, useState } from "react";
+import wallpaper from "./wallpaper.jpg"
 function App() {
   return (
     <>
-      <Navbar />
+      {/* <Navbar /> */}
       <Weather />
-      <Footer />
+      {/* <Footer /> */}
     </>
   );
 }
@@ -42,16 +43,23 @@ function Weather() {
 
   return (
     <>
-      <div className="mt-6 flex justify-center gap-2	">
-        <label className="content-center mr-2">Enter the city: </label>
-        <input type="text" ref={cityRef} />
-        <button className="ml-6 p-2 bg-slate-400 px-8" onClick={handleFetchWeather}>Search</button>
-      </div>
+      <div className="bg-[url('wallpaper.jpg')] h-screen pt-6">
+        <div className="mt-6 flex justify-center gap-2	">
+          <label className="content-center mr-2 text-white">Enter the city: </label>
+          <input type="text" ref={cityRef} />
+          <button className="ml-6 p-2 bg-gray-50 px-8" onClick={handleFetchWeather}>Search</button>
+        </div>
 
-      <div className="mt-14 grid justify-center grid-cols-12">
-        <div className="col-start-5 col-end-9 col-span-4 bg-slate-400 justify-items-center py-14">
-          <p>City name: {city ? city : ""} </p>
-          <p className="py-5">Temperature: {weather ? weather : ""} °C</p>
+        <div className="pt-14 grid justify-center grid-cols-12 grid-rows-6 h-1/2 ">
+          <div className="text-white rounded-md h-full col-start-5 col-end-9 col-span-4 row-start-1 row-end-7 row-span-6 backdrop-blur-sm bg-gray-50/20 justify-items-center py-14">
+            <p>City name: {city ? city : ""} </p>
+            <p className="py-5">Temperature: {weather ? weather : ""} °C</p>
+          </div>
+        </div>
+
+        <div className="text-white flex justify-between fixed bottom-0 py-4 px-4 w-full">
+          <p>Developed by Deepak Kodi</p>
+          <p>Photo by Nabil Naidu on Unsplash</p>
         </div>
       </div>
     </>
@@ -59,16 +67,16 @@ function Weather() {
 }
 
 
-function Navbar() {
-  return (
-    <h3 className="bg-slate-400 text-2xl py-6 px-4">Weather Forecast</h3>
-  );
-}
+// function Navbar() {
+//   return (
+//     <h3 className="bg-slate-400 text-2xl py-6 px-4">Weather Forecast</h3>
+//   );
+// }
 
-function Footer() {
-  return (
-    <p className="bg-slate-400 py-6 px-4 fixed bottom-0 w-full text-center">Created by Deepak Kodi</p>
-  );
-}
+// function Footer() {
+//   return (
+//     <p className="bg-slate-400 py-6 px-4 fixed bottom-0 w-full text-center">Created by Deepak Kodi</p>
+//   );
+// }
 
 export default App;
